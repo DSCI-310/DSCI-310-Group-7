6f3acc7c2bdd165960ba9c5714e44d711313a010
Weihao Sun <sunweihao2019@163.com>
2022-03-12 11:49:23 -0800

    Created test_para_optimize

--- src/para_optimize.py
+++ src/para_optimize.py
@@ -1,7 +1,7 @@
 #' Optimize hyper-parameters for a model
 #'
 #'
-#' @param mod A model already defined to be optimized (e.g. KNN).
+#' @param mod A model already defined to be optimized (e.g. KNN). Models are one of: KNN, DecisionTree, SVM, LogisticRegression
 #' @param params A dictionary of hyper-parameters to be evaluated. 
 #'               Keys are hyper-parameter names. Values are the hyper-parameter values
 #'               (e.g. dict(n_neighbors=list(range(1, 21)))).
@@ -18,9 +18,18 @@
 #'
 #' @examples
 #' para_optimize(knn, param_grid, 5)
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+
 Def para_optimize(mod, params, n, X_train, y_train):
-    grid = GridSearchCV(mod, params, cv=n, scoring='accuracy')
-    grid.fit(X_train, y_train)
-    best = grid.best_params_
-    return best
+    if isinstance(mod, KNeighborsClassifier)|isinstance(mod,DecisionTreeClassifier)|isinstance(mod,svm)|isinstance(mod,LogisticRegression):
+        grid = GridSearchCV(mod, params, cv=n, scoring='accuracy')
+        grid.fit(X_train, y_train)
+        best = grid.best_params_
+        return best
+    else:
+        return("The model is invalid.")
 
