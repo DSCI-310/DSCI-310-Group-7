4387695b5aa2cd68beb709166e03401a1b38f19f
poddarswakhar <poddarswakhar97@gmail.com>
2022-03-12 16:04:25 -0800

    std_acc fix

--- src/std_acc.py
+++ src/std_acc.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 
 
 # ' generate standard deviation of predicted array and observed array associated with each k
--- tests/test_std_acc.py
+++ tests/test_std_acc.py
@@ -1,7 +1,9 @@
 import unittest
 import numpy as np
+import pandas as pd
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 from src.std_acc import *
 
@@ -11,9 +13,11 @@ feature = df_iris[["Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"]
 X = feature
 y = df_iris["Species"]
 
+yhat = np.zeros(10)
+
 # function input for the test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
-for n in range(1, Ks):
+for n in range(1, 50):
     # Train Model and Predict
     decTree = DecisionTreeClassifier(criterion="entropy", max_depth=n)
     decTree.fit(X_train, y_train)
@@ -22,13 +26,23 @@ for n in range(1, Ks):
 msg = "expected value and exact output is not equal!"
 
 # expected out for the test
-std_acc = np.zeros((Ks-1))
+std_acc = np.zeros((50 - 1))
 
-for n in range(1, Ks):
+std_acc_expected = np.zeros((50 - 1))
+
+for n in range(1, 50):
     std_acc_expected[n - 1] = np.std(yhat == y_test) / np.sqrt(yhat.shape[0])
 
-class TestStdAcc(unittest.TestCase):
-    def test_stdAcc(self):
-        std_acc_generated = stdAcc(yhat, y_test, Ks)
-        for i in range(1, Ks):
-            self.assertEqual(std_acc[i], std_acc_generated[i])
+
+def test_stdacc():
+    std_acc = stdAcc(yhat, y_test, 50)
+    if pd.DataFrame(std_acc).equals(pd.DataFrame(std_acc_expected)):
+        assert True
+    else:
+        assert False
+
+# class TestStdAcc(unittest.TestCase):
+#     def test_stdAcc(self):
+#         std_acc_generated = stdAcc(yhat, y_test, 50)
+#         for i in range(1, 50):
+#             self.assertEqual(std_acc[i], std_acc_generated[i])
